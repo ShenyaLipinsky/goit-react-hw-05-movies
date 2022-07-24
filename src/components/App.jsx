60d814b3b295d56e5018d13ddf43fdc8aa441b93
ShenyaLipinsky <shenyalipinskiy@gmail.com>
2022-07-24 17:02:25 +0300

    add Lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
+import { lazy } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
-import Layout from './Layout/Layout';
+// import Layout from './Layout/Layout';
 import Cast from './Pages/FilmDetails/Cast';
 import FilmDetails from './Pages/FilmDetails/FilmDetails';
 import Reviews from './Pages/FilmDetails/Reviews';
-import Home from './Pages/Home/Home';
-import Movies from './Pages/Movies/Movies';
+// import Home from './Pages/Home/Home';
+// import Movies from './Pages/Movies/Movies';
+
+const Layout = lazy(() => import('./Layout/Layout'));
+const Home = lazy(() => import('./Pages/Home/Home'));
+const Movies = lazy(() => import('./Pages/Movies/Movies'));
 
 export const App = () => {
   const location = useLocation();
